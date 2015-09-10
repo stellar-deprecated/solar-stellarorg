ba7e8ba7fd98802fde204d3b739551e4766f2c6d
irisli <github@iris.li>
2015-09-10 15:17:38 -0700

    add tool to generate graphics

---
+++ tools/graphicGenerator.js
@@ -0,0 +1,15 @@
+var glob = require('glob');
+var sizeOf = require('image-size');
+var path = require('path');
+
+// currently only support for .png. .svg will be added when used
+glob(__dirname + '/../graphics/**/*.png', function(er, files) {
+  files.forEach(function(filepath) {
+    var filename = path.basename(filepath);
+    var basename = path.basename(filepath, '.png');
+    var dimensions = sizeOf(filepath);
+    console.log('.so-graphic-' + basename +
+      ' { @include so-graphic("' + filename +
+      '", ' + dimensions.width + ', ' + dimensions.height + '); @extend .so-graphic; }');
+  })
+})
